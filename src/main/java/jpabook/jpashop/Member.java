0e018c816f9c0525d382ccde7b96d3cb02cad190
parkwoongki <sbe03005@naver.com>
2020-09-25 20:37:54 +0900

    feat: Add JPAShop Project

---
+++ src/main/java/jpabook/jpashop/Member.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+}
